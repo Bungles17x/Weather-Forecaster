a32300a0ac37f6a93f6dc8ca2e8101fdfbedb26e
Daniel <lildclaycomb@gmail.com>
2026-03-14 21:08:47 -0400

    Add Routes to App.jsx to make navbar navigation buttons work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import WeatherApp from './components/WeatherApp'
+import HomePage from './pages/HomePage'
+import ForecastPage from './pages/ForecastPage'
+import MapPage from './pages/MapPage'
+import SettingsPage from './pages/SettingsPage'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <WeatherApp />
+        <Routes>
+          <Route path="/" element={<WeatherApp />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/forecast" element={<ForecastPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
       </div>
     </BrowserRouter>
   )
